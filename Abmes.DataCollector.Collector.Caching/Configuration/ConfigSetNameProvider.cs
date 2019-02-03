b4d1f038ff0785e0ee2b6bfd960bace19ac88e8e
Lyubomir Tonchev <lubo@semba.bg>
2019-02-03 23:15:01 +0200

    caching

---
+++ Abmes.DataCollector.Collector.Caching/Configuration/ConfigSetNameProvider.cs
@@ -0,0 +1,29 @@
+﻿using Abmes.DataCollector.Collector.Common.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abmes.DataCollector.Collector.Caching.Configuration
+{
+    public class ConfigSetNameProvider : IConfigSetNameProvider
+    {
+        private readonly IConfigSetNameProvider _configSetNameProvider;
+        private string _configSetName;
+
+        public ConfigSetNameProvider(
+            IConfigSetNameProvider configSetNameProvider)
+        {
+            _configSetNameProvider = configSetNameProvider;
+        }
+
+        public string GetConfigSetName()
+        {
+            if (string.IsNullOrEmpty(_configSetName))
+            {
+                _configSetName = _configSetNameProvider.GetConfigSetName();
+            }
+
+            return _configSetName;
+        }
+    }
+}
--- Abmes.DataCollector.Collector.Caching/ContainerRegistrations.cs
+++ Abmes.DataCollector.Collector.Caching/ContainerRegistrations.cs
@@ -11,9 +11,11 @@ namespace Abmes.DataCollector.Collector.Caching
             builder.RegisterType<ConfigFileCache>().As<IConfigFileCache>().SingleInstance();
 
             builder.RegisterType<Caching.Configuration.ConfigFileProvider>().Named<IConfigProvider>("CachingDecorator");
-
             builder.RegisterDecorator<IConfigProvider>((x, inner) => x.ResolveNamed<IConfigProvider>("CachingDecorator", TypedParameter.From(inner)), "loggingAmazon").As<IConfigProvider>();
             //builder.RegisterDecorator<IConfigProvider>((x, inner) => x.ResolveNamed<IConfigProvider>("CachingDecorator", TypedParameter.From(inner)), "loggingAzure").As<IConfigProvider>();
+
+            builder.RegisterType<Caching.Configuration.ConfigSetNameProvider>().Named<IConfigSetNameProvider>("CachingDecorator");
+            builder.RegisterDecorator<IConfigSetNameProvider>((x, inner) => x.ResolveNamed<IConfigSetNameProvider>("CachingDecorator", TypedParameter.From(inner)), "logging").As<IConfigSetNameProvider>().SingleInstance();
         }
     }
 }
