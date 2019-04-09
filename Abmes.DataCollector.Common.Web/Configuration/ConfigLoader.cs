9bbeb9f470dc53b11f923abc6e7c839a09279206
Lyubomir Tonchev <lubo@semba.bg>
2019-04-09 12:40:22 +0300

    config from url support

--- Abmes.DataCollector.Common.Web/Configuration/ConfigLoader.cs
+++ Abmes.DataCollector.Common.Web/Configuration/ConfigLoader.cs
@@ -33,7 +33,7 @@ namespace Abmes.DataCollector.Common.Web.Configuration
         {
             var bracketPos = location.IndexOf("[");
             var headers = (bracketPos >= 0) ? GetHeaders(location.Substring(bracketPos)) : null;
-            var url = location.Substring(0, bracketPos);
+            var url = location.Substring(0, bracketPos).TrimEnd('/') + "/" + configName;
 
             return await HttpUtils.GetStringAsync(url, headers, null, null, cancellationToken);
         }
