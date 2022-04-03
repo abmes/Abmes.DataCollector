01a4398949ca6716212ef48499b37ce93a539f41
Lyubomir Tonchev <luboice@gmail.com>
2022-04-03 20:26:31 +0300

    * filter

--- Abmes.DataCollector.Collector.Common/Configuration/FilteredDataCollectionsConfigProvider.cs
+++ Abmes.DataCollector.Collector.Common/Configuration/FilteredDataCollectionsConfigProvider.cs
@@ -10,20 +10,20 @@ namespace Abmes.DataCollector.Collector.Common.Configuration
 {
     public class FilteredDataCollectionsConfigProvider : IDataCollectionsConfigProvider
     {
-        private readonly IDataCollectionsFilterProvider _dataFilterProvider;
+        private readonly IDataCollectionsFilterProvider _dataCollectionsFilterProvider;
         private readonly IDataCollectionsConfigProvider _dataCollectionsConfigProvider;
 
         public FilteredDataCollectionsConfigProvider(
             IDataCollectionsFilterProvider dataFilterProvider,
             IDataCollectionsConfigProvider dataCollectionsConfigProvider)
         {
-            _dataFilterProvider = dataFilterProvider;
+            _dataCollectionsFilterProvider = dataFilterProvider;
             _dataCollectionsConfigProvider = dataCollectionsConfigProvider;
         }
 
         public async Task<IEnumerable<DataCollectionConfig>> GetDataCollectionsConfigAsync(string configSetName, CancellationToken cancellationToken)
         {
-            var filter = await _dataFilterProvider.GetDataCollectionsFilterAsync(cancellationToken);
+            var filter = await _dataCollectionsFilterProvider.GetDataCollectionsFilterAsync(cancellationToken);
             var result = await _dataCollectionsConfigProvider.GetDataCollectionsConfigAsync(configSetName, cancellationToken);
             return result.Where(x => DataMatchesFilter(x, filter));
         }
@@ -31,7 +31,8 @@ namespace Abmes.DataCollector.Collector.Common.Configuration
         private bool DataMatchesFilter(DataCollectionConfig config, string filter)
         {
             return 
-                string.IsNullOrEmpty(filter) || 
+                string.IsNullOrEmpty(filter) ||
+                (filter == "*") ||
                 filter.Split(';', ',')
                 .Where(x => !string.IsNullOrEmpty(x))
                 .Select(x => x.Trim())
