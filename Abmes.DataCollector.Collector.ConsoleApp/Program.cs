e234f4eec1f420c925b7fc915ffbda7d2d34735e
Ventsislav Karaivanov <karaivanov@gmail.com>
2022-10-07 18:46:10 +0300

    refactoring

--- Abmes.DataCollector.Collector.ConsoleApp/Program.cs
+++ Abmes.DataCollector.Collector.ConsoleApp/Program.cs
@@ -1,11 +1,3 @@
 ﻿using Abmes.DataCollector.Collector.ConsoleApp.Initialization;
 
-namespace Abmes.DataCollector.Collector.ConsoleApp;
-
-class Program
-{
-    static async Task<int> Main(string[] args)
-    {
-        return await Initializer.GetMainService().MainAsync(CancellationToken.None);
-    }
-}
+return await Initializer.GetMainService().MainAsync(CancellationToken.None);
