ff81286a2bdc3b04ef19a378914dd6008787560d
Ventsislav Karaivanov <karaivanov@gmail.com>
2025-08-08 12:43:16 +0300

    utils unification across solutions

--- Abmes.Utils.Options.DependencyInjection/OptionsAdapterServiceCollectionExtensions.cs
+++ Abmes.Utils.Options.DependencyInjection/OptionsAdapterServiceCollectionExtensions.cs
@@ -4,12 +4,12 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class OptionsAdapterServiceCollectionExtensions
 {
-    public static IServiceCollection AddOptionsAdapter<TOptions>(this IServiceCollection services) where TOptions : class, new()
+    public static IServiceCollection AddOptionsAdapter<TOptions>(this IServiceCollection services) where TOptions : class
     {
         return services.AddOptionsAdapter<TOptions, TOptions>();
     }
 
-    public static IServiceCollection AddOptionsAdapter<TTo, TFrom>(this IServiceCollection services) where TTo : class where TFrom : class, TTo, new()
+    public static IServiceCollection AddOptionsAdapter<TTo, TFrom>(this IServiceCollection services) where TTo : class where TFrom : class, TTo
     {
         services.AddOptions();
         return services.AddAdapter<TTo, IOptions<TFrom>>(o => o.Value);
