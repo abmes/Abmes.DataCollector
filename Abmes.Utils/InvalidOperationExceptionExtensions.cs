e14041cddfc429fb269e6c13b53e3b2bc8f66033
Ventsislav Karaivanov <vencyk@users.noreply.github.com>
2025-10-29 17:33:35 +0200

    utils unification across solutions

---
+++ Abmes.Utils/InvalidOperationExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Abmes.Utils;
+
+public static class InvalidOperationExceptionExtensions
+{
+    public static void ThrowIf([DoesNotReturnIf(true)] bool condition, [CallerArgumentExpression(nameof(condition))] string? conditionText = null)
+    {
+        if (condition)
+        {
+            throw new InvalidOperationException(conditionText);
+        }
+    }
+
+    public static void ThrowIfNot([DoesNotReturnIf(false)] bool condition, [CallerArgumentExpression(nameof(condition))] string? conditionText = null)
+    {
+        if (!condition)
+        {
+            throw new InvalidOperationException($"'{conditionText}' is not true");
+        }
+    }
+}
